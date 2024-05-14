aae01f6353dfc00d02c507a2c5dbdb886601520c
LordWilliam <willjordan05@icloud.com>
2024-05-14 09:57:55 -0700

    Portfolio page work for real.

--- wopersonalsite/.contentlayer/generated/portfolio/_index.mjs
+++ wopersonalsite/.contentlayer/generated/portfolio/_index.mjs
@@ -4,5 +4,6 @@ import portfolio__testProject_1Md from './portfolio__test-project-1.md.json' ass
 import portfolio__testProject_2Md from './portfolio__test-project-2.md.json' assert { type: 'json' }
 import portfolio__testProjectMd from './portfolio__test-project.md.json' assert { type: 'json' }
 import portfolio__testMd from './portfolio__test.md.json' assert { type: 'json' }
+import portfolio__test1Md from './portfolio__test1.md.json' assert { type: 'json' }
 
-export const allPortfolios = [portfolio__testProject_1Md, portfolio__testProject_2Md, portfolio__testProjectMd, portfolio__testMd]
+export const allPortfolios = [portfolio__testProject_1Md, portfolio__testProject_2Md, portfolio__testProjectMd, portfolio__testMd, portfolio__test1Md]
---
+++ wopersonalsite/content/portfolio/test1.md
@@ -0,0 +1,12 @@
+---
+type: Portfolio
+image: 
+    src: "/images/portfolio/doom.jpg" 
+    width: 1920
+    height: 1080
+date: 2024-05-11
+tags: []
+slug: testportfolio1
+title: Test Portfolio 1
+---
+super test
