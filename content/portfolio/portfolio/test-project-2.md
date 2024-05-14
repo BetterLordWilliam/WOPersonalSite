5ed5d7c6bdb1daa9f90852ddba1ef498143a8a75
LordWilliam <willjordan05@icloud.com>
2024-05-13 21:23:29 -0700

    Added script to read data from the notion online database. Setup .env such that these resources can safely be accessed by the code.

---
+++ content/portfolio/portfolio/test-project-2.md
@@ -0,0 +1,19 @@
+---
+notionId: 65ebc367-a96d-4ecc-8870-0ea8053d858b
+type: Portfolio
+date: 2024-05-15
+tags:
+  - Test3
+  - Test
+  - Test2
+image:
+  src: /images/portfolio/Demon rat.png
+  width: 231
+  height: 185
+location: Somewhere
+enabled: true
+title: Test Project 2
+slug: test-project-2
+
+---
+undefined
--- wopersonalsite/scripts/notion.js
+++ wopersonalsite/scripts/notion.js
@@ -3,27 +3,30 @@
 require("dotenv").config();
 const NotionParse = require("@kodaps/notion-parse");
 
-console.log(process.env.NOTION_SECRET);
-console.log(process.env.NOTION_PORTFOLIO_DATABASE_ID);
-
 /**
  * go:                              code which retrieves notion portfolio content
  * NOTION_SECRET:                   the access to the Notion Page
  * NOTION_PORTFOLIO_DATABASE_ID:    the Notion Page that stores the actual project document content
  */
-const go = async() => {
-    if (process.env.NOTION_SECRET) {
-        // @ts-ignore
-        await NotionParse.parseNotion(process.env.NOTION_SECRET, "../content", [
-            {
-                databaseId:     process.env.NOTION_PORTFOLIO_DATABASE_ID || "",
-                contentType:    "portfolio",  
-            },
-        ])
-    }
-}
+const go = async () => {
 
-// Test
-go().then(()=>{
-    console.log("done");
-})
+  if (process.env.NOTION_SECRET) {
+    await NotionParse.parseNotion(process.env.NOTION_SECRET, './content', [
+      {
+        databaseId: process.env.NOTION_PORTFOLIO_DATABASE_ID || '',
+        contentType: 'Portfolio'
+      },
+      /*{
+        databaseId: process.env.NOTION_POST_DATABASE_ID || '',
+        contentType: 'Post',
+        languageField: 'lang',
+        filterFields: [ 'translation', 'createdAt', 'status', 'Type']
+      },*/
+    ])
+  }
+
+};
+
+go().then(() => {
+  console.log('Done');
+});
