4de243c8f68f7cd31e54838771c879c8006e8bca
LordWilliam <willjordan05@icloud.com>
2024-05-19 23:14:18 -0700

    Header styles done, can't remember if I already said this but the footer styles are also done. Will look at this again tomorrow. TO-DO; write the content for the home page and then build + deploy.

--- wopersonalsite/components/Header.tsx
+++ wopersonalsite/components/Header.tsx
@@ -5,13 +5,17 @@ const portfolio: string = "./portfolio";
 const contact: string = "./contact";
 
 export const Header:React.FC = () => {
-    return  <div className="mx-auto max-w-5xl grid grid-cols-2 m-8">
-        <img src="../images/TitleIcon.png" alt="website_icon"></img>
-        <div className="pb-2 self-center">
-        <div className="max-w-72 md:w-full grid grid-cols-3 gap-1">
-            <ExternalLinkButton url={home} buttonText={"Home"} ></ExternalLinkButton>
-            <ExternalLinkButton url={portfolio} buttonText={"Projects"} ></ExternalLinkButton>
-            <ExternalLinkButton url={contact} buttonText={"Contact"} ></ExternalLinkButton>
+    return  <div className="mx-auto max-w-5xl">
+        <div className="mb-8 mx-4 grid md:grid-cols-2">
+        <div className="flex justify-center">
+        <img className="" src="../images/TitleIcon.png" alt="website_icon"></img>
+        </div>
+        <div className="flex md:justify-end justify-center">
+            <div className="pb-2 self-center max-w-72 md:w-full grid grid-cols-3 gap-1">
+                <ExternalLinkButton url={home} buttonText={"Home"} ></ExternalLinkButton>
+                <ExternalLinkButton url={portfolio} buttonText={"Projects"} ></ExternalLinkButton>
+                <ExternalLinkButton url={contact} buttonText={"Contact"} ></ExternalLinkButton>
+            </div>
         </div>
         </div>
     </div>
