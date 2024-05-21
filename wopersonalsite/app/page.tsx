833329d67752b065d5da00716751f223accfdc50
LordWilliam <willjordan05@icloud.com>
2024-05-20 19:07:21 -0700

    Spelling corrections and minor style changes to the landing page. Done.

--- wopersonalsite/app/page.tsx
+++ wopersonalsite/app/page.tsx
@@ -18,18 +18,23 @@ const HomeIndex: NextPage = () => {
             </div>
             <div className="mx-4 rounded flex flex-wrap md:flex-nowrap">
                 <div className="max-w-l md:max-w-xl rounded p-4 bg-closeiToBlack relative">
-
-                    <br/>
+                    
+                    <h3 className="mb-4 text-white">I am Will Otterbein, a programmer from British Columbia, Canada.</h3>
                     <p>
-                    Furthermore, you will find in the footer links to my social media which you can use to contact me.
-                    If you&apos;d prefer, I&apos;ve also developed a contact page that you can use to connect via email.
+                    Since highschool, I've busied myself with the thrills of programming. 
+                    I've always enjoyed the engagement received from solving complex problems, 
+                    and do my best to elegantly code the solutions to the various problems that I have encountered.
                     </p>
                     <br/>
+                    <p>
+                    If you want to get in touch, you will find in the footer links to my social media which you can use to contact me.
+                    If you&apos;d prefer, I&apos;ve also developed a contact page that you can use to connect via email.
+                    </p>
                     <br/>
                     <p>
                     I sincerely hope that you enjoy browsing the highlights of my work as a software developer and student.
                     </p>
-                    <br/><br/><hr className="border-2 border-closeishToBlack"/><br/><br/>
+                    <br/><hr className="border-2 border-closeishToBlack"/><br/><br/>
                     <div className="left-0 bottom-0">
                         <p>
                         This site was developed using NextJS and Tailwindcss. It is hosted via the Vercel hosting platform for NextJS web-applications.
@@ -37,7 +42,7 @@ const HomeIndex: NextPage = () => {
                     </div>
 
                 </div>
-                <div className="hidden max-w-40 overview-hidden ml-auto md:block">
+                <div className="hidden max-w-48 overview-hidden ml-auto md:block">
                     <img className="rounded" src={meImage_Small} alt="me"></img>
                 </div>
                 <br/>
